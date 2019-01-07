57f0dcbdb46e5e92bb25d16f3320e4508f98ddb6
Jake Casto <jake@snaju.com>
2019-01-07 14:47:12 -0500

    Added support for "standard" functions, fixed variable ref bug.
    I also cut the compile time by 0.001 (almost a 33% reduction).

--- src/onembsite/onembcode/Parser.php
+++ src/onembsite/onembcode/Parser.php
@@ -34,7 +34,7 @@ class Parser {
 	protected function loadDefaultFunctions()
 	{
 		$this->functions['print'] = function($mixed) { 
-			echo is_array($mixed) ? json_encode($mixed) : (string) $mixed;
+			echo (is_array($mixed) ? json_encode($mixed) : (string) $mixed) . PHP_EOL;
 		};
 		$this->functions['dump'] = function($mixed) { 
 			var_dump($mixed);
@@ -61,10 +61,11 @@ class Parser {
 		$this->lexer->add('/^((\[|\{)[a-z,0-9\ \"\:]+(\]|\}))/', 'T_ARRAY|T_VARIABLE_VALUE');
 		$this->lexer->add('/^(((?![\ ])([a-z]+))\(([0-9a-z\,\ ])+\))/', 'T_FUNCTION_RESULT|T_VARIABLE_VALUE');
 		$this->lexer->add('/^((?![\ \=\"0-9\(\)]+)(\&)(\b(\w+(?![\(]))([\.]\w+)))/', 'T_NESTED_VARIABLE_REFERENCE|T_VARIABLE_VALUE');
-		$this->lexer->add('/^(?![\ \;\=\"0-9\(\)]+)(\&)(\b(\w+(?![\(])))(?!\w)/', 'T_VARIABLE_REFERENCE|T_VARIABLE_VALUE');
+		$this->lexer->add('/^(?![\ \;\=\"0-9\(\)]+)(\&)(\b(\w+(?![\(]))\b)(?!\w)/', 'T_VARIABLE_REFERENCE|T_VARIABLE_VALUE');
 		$this->lexer->add('/^(\+|\*|\\|\-)/', 'T_ASSIGNMENT_OPERATOR');
 		$this->lexer->add('/^(=)/', 'T_EQUALS');
 		$this->lexer->add('/^(;)/', 'T_EOL');
+		$this->lexer->add('/^(((?![\ ])([a-z]+))\(([0-9a-z\,\ \"\&\{\}\_\-\+\*])+\));/', 'T_FUNCTION');
 
 		$tokens = $this->lexer->run();
 		while(($token = $tokens->next()) && $token !== null)
@@ -152,7 +153,7 @@ class Parser {
 								$variable_value->match = call_user_func_array($this->functions[$function_name], array_map('trim', $base_params));
 							break;
 							case 'T_VARIABLE_REFERENCE':
-								$variable_ref = strtr($variable_value, ['&' => '']);
+								$variable_ref = strtr($variable_value->match, ['&' => '']);
 								if(!isset($this->variables[$variable_ref]))
 								{
 									throw new \Exception('RuntimeError: Attempt to access undefined variable ' . $variable_ref . ' on line ' . $variable_value->line);
@@ -195,8 +196,172 @@ class Parser {
 					$this->variables[$variable_name->match] = $variable_value->match;
 					$tokens->setIndex($variable_value->index);
 				break;
+				case 'T_FUNCTION':
+					preg_match('/^([a-z]+)/', $token->match, $function_name_matches);
+					
+					$function_name = $function_name_matches[0];
+					if(!isset($this->functions[$function_name]))
+					{
+						throw new \Exception('RuntimeError: Call to undefined function ' . $function_name . ' on line ' . $token->line);
+					}
+
+					preg_match('/([0-9a-z\,\ \&\{\}\"\+\-\*]+)/', substr($token->match, strlen($function_name)), $function_param_matches);
+
+					$function_params = @$function_param_matches[0];
+					if($function_params === null)
+					{
+						throw new \Exception('RuntimeError: Unable to parse function params for function ' . $function_name . ' on line ' . $token->line);
+					}
+
+					$params = $this->parseValuesFromArrayForFunctionParams(explode(',', str_replace(['(', ')'], '', $function_params)));
+					call_user_func_array($this->functions[$function_name], $params);
+				break;
 			}
 		}
+	}
+
+	protected function parseValuesFromArrayForFunctionParams(array $base)
+	{
+		$lexer = new Lexer($base);
+
+		$lexer->add('/^( )/', 'T_WHITESPACE');
+		$lexer->add('/^("([^\\"]+|\\.)*")/', 'T_STRING|T_VARIABLE_VALUE');
+		$lexer->add('/^(?![\ \.]+)([0-9]+)$/', 'T_INTEGER|T_VARIABLE_VALUE');
+		$lexer->add('/^([0-9]*\.?[0-9]+)/', 'T_FLOAT|T_VARIABLE_VALUE');
+		$lexer->add('/^(true|false)+/', 'T_BOOLEAN|T_VARIABLE_VALUE');
+		$lexer->add('/^(((?![\ ])([a-z]+))\(([0-9a-z\,\ ])+\))/', 'T_FUNCTION_RESULT|T_VARIABLE_VALUE');
+		$lexer->add('/^((?![\ \=\"0-9\(\)]+)(\&)(\b(\w+(?![\(]))([\.]\w+)))/', 'T_NESTED_VARIABLE_REFERENCE|T_VARIABLE_VALUE');
+		$lexer->add('/^((?![\ \=\"0-9\(\)]+)(\&)(\b(\w+(?![\(])))\b)(?!\w)/', 'T_VARIABLE_REFERENCE|T_VARIABLE_VALUE');
+		$lexer->add('/^(\+|\*|\\|\-)/', 'T_ASSIGNMENT_OPERATOR');
+
+		$tokens = $lexer->run();
+		$values = [];
+		while(($token = $tokens->next()) && $token !== null)
+		{
+			$variable_value = $token;//in_array('T_VARIABLE_VALUE', explode('|', $token->type));
+
+			// WE CHECK IF THE NEXT ONE IS A LOGICAL OPERATOR SO IT CAN BE PARSED CORRECTLY
+			// THIS CAUSES PERFORMANCE ISSUES, USING A FUNCTION FOR MATH IS FASTER?
+			$tokens->setIndex($variable_value->index);
+			$operator = $tokens->next('T_WHITESPACE', 1);
+			if($operator !== null && in_array('T_ASSIGNMENT_OPERATOR', explode('|', $operator->type)))
+			{
+				$next = $tokens->next('T_WHITESPACE');
+				if($variable_value->type !== 'T_INTEGER|T_VARIABLE_VALUE' && $variable_value->type !== 'T_FLOAT|T_VARIABLE_VALUE')
+				{
+					throw new \Exception('Parse Error: Unexpected ' . $variable_value->match . ' on line ' . $variable_value->line);
+				}
+				if($next->type !== 'T_INTEGER|T_VARIABLE_VALUE' && $next->type !== 'T_FLOAT|T_VARIABLE_VALUE')
+				{
+					throw new \Exception('Parse Error: Unexpected ' . $next->match . ' on line ' . $variable_value->line);
+				}
+
+				switch($operator->match)
+				{
+					case '+':
+						$variable_value->match = $variable_value->match + $next->match;
+					break;
+					case '-':
+						$variable_value->match = $variable_value->match - $next->match;
+					break;
+					case '*':
+						$variable_value->match = $variable_value->match * $next->match;
+					break;
+					case '/':
+						$variable_value->match = $variable_value->match / $next->match;
+					break;
+				}
+			}
+			else
+			{
+				$type = (explode('|', $variable_value->type))[0];
+				switch($type)
+				{
+					case 'T_STRING':
+						$variable_value->match = strtr($variable_value->match, ['"' => '']);
+					break;
+					case 'T_INTEGER':
+						$variable_value->match = (integer) $variable_value->match;
+					break;
+					case 'T_FLOAT';
+						$variable_value->match = (float) $variable_value->match;
+					break;
+					case 'T_BOOLEAN':
+						$variable_value->match = ($variable_value->match === 'true') ? true : false;
+					break;
+					case 'T_ARRAY':
+						$variable_value->match = json_decode($variable_value->match, true);
+						if(!is_array($variable_value->match))
+						{
+							throw new \Exception('Parse Error: Unable to parse array on line ' . $variable_value->line);
+						}
+					break;
+					case 'T_FUNCTION_RESULT':
+						preg_match('/^([a-z]+)/', $variable_value->match, $function_name_matches);
+						preg_match('/(\()([0-9a-z\,\ ]+)(\))/', $variable_value->match, $function_param_matches);
+						
+
+						$function_name = $function_name_matches[0];
+						if(!isset($this->functions[$function_name]))
+						{
+							throw new \Exception('RuntimeError: Call to undefined function ' . $function_name . ' on line ' . $variable_value->line);
+						}
+
+						$function_params = @$function_param_matches[0];
+						if($function_params === null)
+						{
+							throw new \Exception('RuntimeError: Unable to parse function params for function ' . $function_name . ' on line ' . $variable_value->line);
+						}
+
+						$base_params = explode(',', str_replace(['(', ')'], '', $function_params));
+						$variable_value->match = call_user_func_array($this->functions[$function_name], array_map('trim', $base_params));
+					break;
+					case 'T_VARIABLE_REFERENCE':
+						$variable_ref = strtr($variable_value->match, ['&' => '']);
+						if(!isset($this->variables[$variable_ref]))
+						{
+							throw new \Exception('RuntimeError: Attempt to access undefined variable ' . $variable_ref . ' on line ' . $variable_value->line);
+						}
+
+						$variable_value->match = $this->variables[$variable_ref];
+					break;
+					case 'T_NESTED_VARIABLE_REFERENCE':
+						$variable_ref = strtr($variable_value->match, ['&' => '']);
+
+				        $current_value = $this->variables;
+				        $key_path = explode('.', $variable_ref);
+				        $value = null;
+				        for($i = 0; $i < count($key_path); $i++)
+				        {
+				        	$current_key = $key_path[$i];
+				        	if(!isset($current_value[$current_key]))
+				        	{
+				        		throw new \Exception('RuntimeError: Attempt to access undefined variable ' . $variable_ref . ' on line ' . $variable_value->line);
+				        	}
+
+				            if(!is_array($current_value))
+				            {
+				                throw new \Exception('RuntimeError: Attempt to access undefined variable ' . $variable_ref . ' on line ' . $variable_value->line);
+				            }
+
+				            $current_value = $current_value[$current_key];
+				        }
+
+				        if(is_null($current_value))
+				        {
+				        	throw new \Exception('RuntimeError: Attempt to access undefined variable ' . $variable_ref . ' on line ' . $variable_value->line);
+				        }
+
+				        $variable_value->match = $current_value;
+					break;
+				}
+			}
+
+			$values[] = $variable_value->match;
+			$tokens->setIndex($variable_value->index + 1);
+		}
+
+		return $values;
 	}
 
     public static function getVersion()
--- tests/dump_variables.php
+++ tests/dump_variables.php
@@ -2,7 +2,13 @@
 require_once './vendor/autoload.php';
 
 $script = 'var str_test = "hey";var int_test = 500;var float_test = 100.37;var array_test = {"name": "test"}; var func_test = add(1, 2);var nested_test = &array_test.name;var bool_test = true; var test_assignment = 1 + 4;';
-// $script = 'var func_test = add(1, 2);';
+$script .= 'var func_test = add(1, 2);';
+
+$script .= 'print(1 + 4);';
+
+$script .= 'var assignment = 1 + 4;';
+$script .= PHP_EOL;
+$script .= 'print(&assignment);';
 
 $functions = [];
 $functions['add'] = function($one, $two) { return (int)$one + (int)$two; };
@@ -11,4 +17,4 @@ $parser = new \onembsite\onembcode\Parser($script, $functions);
 $startTime = microtime(true);
 $parser->parse();
 echo "Compile time is: ". (microtime(true) - $startTime) ." seconds" . PHP_EOL;
-//output: Compile time is: 0.0063848495483398 seconds (PHP 7.0, Windows 10 Pro Edition)
+//output: Compile time is: 0.004626989364624 seconds (PHP 7.0, Windows 10 Pro Edition)
